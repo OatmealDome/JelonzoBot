2833c064e2c49ad3cfa87a6e61fbee880ac474d6
OatmealDome <theoatmealdome@gmail.com>
2019-03-30 03:48:43 -0400

    Blitz: Add RomType enum

---
+++ Nintendo/Blitz/RomType.cs
@@ -0,0 +1,17 @@
+namespace Nintendo.Blitz
+{
+    public enum RomType : int
+    {
+        Japan = 0,
+        NorthAmerica,
+        Europe,
+        EveJapan,
+        EveNorthAmerica,
+        EveEurope,
+        TrialJapan,
+        TrialNorthAmerica,
+        TrialEurope,
+        ShowFloor,
+        ShowDL
+    }
+}
