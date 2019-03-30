518e820bf1bfcb2ccf127440d50fa4dbd016edab
OatmealDome <theoatmealdome@gmail.com>
2019-03-29 20:29:43 -0400

    Difference: Add BlitzBcatFileType

---
+++ JelonzoBot/Difference/BlitzBcatFileType.cs
@@ -0,0 +1,17 @@
+namespace JelonzoBot.Difference
+{
+    public enum BlitzBcatFileType : int
+    {
+        VSSetting = 0,
+        SalmonRunSchedule,
+        FestivalByaml,
+        FestivalHapTexture,
+        FestivalIconTexture,
+        FestivalPanelTexture,
+        FestivalDeliModel,
+        FestivalDeliLayout,
+        FestivalGrafittiSettings,
+        DummyFile,
+        Unknown
+    }
+}
