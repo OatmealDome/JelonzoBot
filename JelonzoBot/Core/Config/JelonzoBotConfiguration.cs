3a3bcfbc441c5aa713cf8a0ea40ca964e6bb8746
OatmealDome <theoatmealdome@gmail.com>
2019-04-06 05:39:06 -0400

    Config: Add last download paths to allow old file loading

--- JelonzoBot/Core/Config/JelonzoBotConfiguration.cs
+++ JelonzoBot/Core/Config/JelonzoBotConfiguration.cs
@@ -1,9 +1,16 @@
+using System.Collections.Generic;
 using BcatBotFramework.Core.Config;
+using Nintendo.Blitz;
 
 namespace JelonzoBot.Core.Config
 {
     public class JelonzoBotConfiguration : Configuration
     {
+        public Dictionary<RomType, string> LastDownloadPaths
+        {
+            get;
+            set;
+        }
         
     }
 }
