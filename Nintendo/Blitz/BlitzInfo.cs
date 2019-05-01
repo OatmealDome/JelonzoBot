ee1f2bc5b2102b0c0856533d2731171284ef4faf
OatmealDome <theoatmealdome@gmail.com>
2019-05-01 04:57:48 -0400

    Blitz: Add BlitzInfo for generic information about Blitz

---
+++ Nintendo/Blitz/BlitzInfo.cs
@@ -0,0 +1,23 @@
+using Nintendo.Bcat;
+
+namespace Nintendo.Blitz
+{
+    public static class BlitzInfo
+    {
+        public static Language[] SupportedLanguages = 
+        {
+            Language.Japanese,
+            Language.EnglishUS,
+            Language.SpanishLA,
+            Language.FrenchCA,
+            Language.EnglishUK,
+            Language.SpanishES,
+            Language.FrenchFR,
+            Language.German,
+            Language.Italian,
+            Language.Dutch,
+            Language.Russian
+        };
+        
+    }
+}
