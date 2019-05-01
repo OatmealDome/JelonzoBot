055f9d2cd27c589cc32b1ca24bd15e99c0ea34dc
OatmealDome <theoatmealdome@gmail.com>
2019-05-01 05:09:24 -0400

    Json: Add LatestFestivalManifest to hold the IDs of the latest Splatfests

---
+++ JelonzoBot/Json/LatestFestivalManifest.cs
@@ -0,0 +1,24 @@
+namespace JelonzoBot.Json
+{
+    public class LatestFestivalManifest
+    {
+        public int NorthAmerica
+        {
+            get;
+            set;
+        }
+
+        public int Europe
+        {
+            get;
+            set;
+        }
+
+        public int Japan
+        {
+            get;
+            set;
+        }
+
+    }
+}
