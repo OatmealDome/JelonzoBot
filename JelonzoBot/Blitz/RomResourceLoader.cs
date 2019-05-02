0b344d653c9b50206f43105a09de6688244c1fc4
OatmealDome <theoatmealdome@gmail.com>
2019-05-02 16:33:44 -0400

    RomResourceLoader: Move BYAML loading here

--- JelonzoBot/Blitz/RomResourceLoader.cs
+++ JelonzoBot/Blitz/RomResourceLoader.cs
@@ -6,6 +6,8 @@ using JelonzoBot.Core.Config;
 using LibHac.IO;
 using Nintendo.Blitz;
 using Nintendo.Switch;
+using Syroot.BinaryData;
+using Syroot.NintenTools.Byaml.Dynamic;
 using Syroot.NintenTools.Yaz0;
 
 namespace JelonzoBot.Blitz
@@ -83,6 +85,48 @@ namespace JelonzoBot.Blitz
 
             // Return the array
             return finalData;
+        }
+
+        public static dynamic GetByaml(string romPath)
+        {
+            // Decide the course of action
+            ushort firstBytes;
+            using (MemoryStream stream = new MemoryStream(GetFile(romPath)))
+            using (BinaryDataReader reader = new BinaryDataReader(stream, Encoding.ASCII, true))
+            using (BinaryDataWriter writer = new BinaryDataWriter(stream, Encoding.ASCII, true))
+            {
+                // Read the first two bytes of the file in big endian
+                reader.ByteOrder = ByteOrder.BigEndian;
+                firstBytes = reader.ReadUInt16();
+
+                // Check endianness
+                if (firstBytes == 0x4259) // "BY" (big endian)
+                {
+                    reader.ByteOrder = ByteOrder.BigEndian;
+                }
+                else if (firstBytes == 0x5942) // "YB" (little endian)
+                {
+                    reader.ByteOrder = ByteOrder.LittleEndian;
+                }
+                else // Not a byaml?
+                {
+                    return null;
+                }
+
+                // Force version to 1
+                writer.Seek(0x2, SeekOrigin.Begin);
+                writer.Write((ushort)0x0001);
+
+                // Get the first byte at where the path array offset is
+                reader.Seek(0x10, SeekOrigin.Begin);
+                byte firstByte = reader.ReadByte();
+
+                // Seek back to the beginning
+                stream.Seek(0, SeekOrigin.Begin);
+
+                // Load the byaml
+                return ByamlFile.Load(stream, firstByte == 0xc3, reader.ByteOrder);
+            }
         }
 
     }
