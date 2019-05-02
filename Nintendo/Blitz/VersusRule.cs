5d085173feee5a5a96b3d994c58a53903b405ebe
OatmealDome <theoatmealdome@gmail.com>
2019-05-01 23:03:47 -0400

    VersusRule: Only use internal names

--- Nintendo/Blitz/VersusRule.cs
+++ Nintendo/Blitz/VersusRule.cs
@@ -2,18 +2,11 @@ namespace Nintendo.Blitz
 {
     public enum VersusRule : int
     {
-        None,
-        TurfWar,
-        SplatZones,
-        TowerControl,
-        Rainmaker,
-        ClamBlitz,
-
-        // Blitz internal names
-        cPnt = TurfWar,
-        cVar = SplatZones,
-        cVlf = TowerControl,
-        cVgl = Rainmaker,
-        cVcl = ClamBlitz
+        cNone,
+        cPnt, // Turf War,
+        cVar, // Splat Zones,
+        cVlf, // Tower Control,
+        cVgl, // Rainmaker,
+        cVcl // Clam Blitz
     }
 }
