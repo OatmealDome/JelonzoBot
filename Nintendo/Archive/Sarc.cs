d37604889668aab56c7bc4f3f9c2b80aefc10bcb
OatmealDome <theoatmealdome@gmail.com>
2019-05-02 16:35:23 -0400

    Nintendo: Add untested SARC parser

---
+++ Nintendo/Archive/Sarc.cs
@@ -0,0 +1,181 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using Syroot.BinaryData;
+
+namespace Nintendo.Archive
+{
+    public class Sarc : IEnumerable<KeyValuePair<string, byte[]>>
+    {
+        struct SfatNode
+        {
+            public uint Hash;
+            public int NameOfs;
+            public uint DataOfs;
+            public uint DataLength;
+        }
+
+        private Dictionary<string, byte[]> Files;
+
+        public object this[string key]
+        {
+            get
+            {
+                return Files[key];
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public Sarc(byte[] rawSarc)
+        {
+            using (MemoryStream memoryStream = new MemoryStream(rawSarc))
+            {
+                Read(memoryStream);
+            }
+        }
+
+        public Sarc(Stream stream)
+        {
+            Read(stream);
+        }
+
+        private void Read(Stream stream)
+        {
+            // Create a dictionary to hold the files
+            Files = new Dictionary<string, byte[]>();
+
+            using (BinaryDataReader reader = new BinaryDataReader(stream))
+            {
+                // Set endianness to big by default
+                reader.ByteOrder = ByteOrder.BigEndian;
+
+                // Verify the magic numbers
+                if (reader.ReadString(4) != "SARC")
+                {
+                    throw new Exception("Not a SARC file");
+                }
+
+                // Skip the header length
+                reader.Seek(4);
+
+                // Check the byte order mark to see if this file is little endian
+                if (reader.ReadUInt16() == 0xFFFE)
+                {
+                    // Set the endiannes to little
+                    reader.ByteOrder = ByteOrder.LittleEndian;
+                }
+
+                // Check the file length
+                if (reader.ReadUInt32() != reader.Length)
+                {
+                    throw new Exception("SARC is possibly corrupt, invalid length");
+                }
+
+                // Read the beginning of data offset
+                uint dataBeginOfs = reader.ReadUInt32();
+
+                // Verify the version
+                if (reader.ReadUInt16() != 0x0100)
+                {
+                    throw new Exception("Unsupported SARC version");
+                }
+
+                // Seek past the reserved area
+                reader.Seek(2);
+
+                // Verify the SFAT magic numbers
+                if (reader.ReadString(4) != "SFAT")
+                {
+                    throw new Exception("Could not find SFAT section");
+                }
+
+                // Skip the header length
+                reader.Seek(2);
+
+                // Read the node count and hash key
+                ushort nodeCount = reader.ReadUInt16();
+                uint hashKey = reader.ReadUInt32();
+
+                // Read every node
+                List<SfatNode> nodes = new List<SfatNode>();
+                for (ushort i = 0; i < nodeCount; i++)
+                {
+                    // Read the node details
+                    uint hash = reader.ReadUInt32();
+                    ushort fileAttrs = reader.ReadUInt16();
+                    int nameOfs = reader.ReadUInt16() * 4;
+                    uint nodeDataBeginOfs = reader.ReadUInt32();
+                    uint nodeDataEndOfs = reader.ReadUInt32();
+
+                    // Create a new SfatNode
+                    nodes.Add(new SfatNode()
+                    {
+                        Hash = hash,
+                        NameOfs = (fileAttrs & 0x0100) == 0x0100 ? nameOfs : int.MaxValue,
+                        DataOfs = nodeDataBeginOfs,
+                        DataLength = nodeDataEndOfs - nodeDataBeginOfs
+                    });
+                }
+
+                // Verify the SFNT magic numbers
+                if (reader.ReadString(4) != "SFNT")
+                {
+                    throw new Exception("Could not find SFNT section");
+                }
+
+                // SKip header length and reserved area
+                reader.Seek(4);
+
+                // Get the file name beginning offset
+                long nameBeginOfs = reader.Position;
+
+                // Read each file using its SfatNode
+                foreach (SfatNode node in nodes)
+                {
+                    // Read the filename
+                    string filename;
+
+                    // Check if there is a name offset
+                    if (node.NameOfs != int.MaxValue)
+                    {
+                        // Read the name at this position
+                        using (reader.TemporarySeek(nameBeginOfs + node.NameOfs, SeekOrigin.Begin))
+                        {
+                            filename = reader.ReadString(BinaryStringFormat.ZeroTerminated);
+                        }
+                    }
+                    else
+                    {
+                        // Use the hash as the name
+                        filename = node.Hash.ToString("X8") + ".bin";
+                    }
+
+                    // Read the file data
+                    byte[] fileData;
+                    using (reader.TemporarySeek(dataBeginOfs + node.DataOfs, SeekOrigin.Begin))
+                    {
+                        fileData = reader.ReadBytes((int)node.DataLength);
+                    }
+
+                    // Add the file to the dictionary
+                    Files.Add(filename, fileData);
+                }
+            }
+        }
+
+        public IEnumerator<KeyValuePair<string, byte[]>> GetEnumerator()
+        {
+            return Files.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+        
+    }
+}
