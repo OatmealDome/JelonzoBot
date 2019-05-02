9282865937ab518d4beb74ad55366d60471d426f
OatmealDome <theoatmealdome@gmail.com>
2019-05-02 18:15:00 -0400

    RomResourceLoader: Add methods to deserialize BYAMLs

--- JelonzoBot/Blitz/RomResourceLoader.cs
+++ JelonzoBot/Blitz/RomResourceLoader.cs
@@ -9,6 +9,7 @@ using Nintendo.Blitz;
 using Nintendo.Switch;
 using Syroot.BinaryData;
 using Syroot.NintenTools.Byaml.Dynamic;
+using Syroot.NintenTools.Byaml.Serialization;
 using Syroot.NintenTools.Yaz0;
 
 namespace JelonzoBot.Blitz
@@ -111,6 +112,31 @@ namespace JelonzoBot.Blitz
             }
         }
 
+        public static T GetByamlDeserializedFromLocal<T>(byte[] ramByaml)
+        {
+            // Load the BYAML from a MemoryStream
+            using (MemoryStream stream = new MemoryStream(ramByaml))
+            {
+                // Get the byaml settings
+                ByamlSettings settings = PrepareByaml(stream);
+
+                // Read the byaml
+                return LoadByamlSerialized<T>(settings, stream);
+            }
+        }
+
+        public static T GetByamlDeserializedFromRom<T>(string romPath)
+        {
+            using (Stream stream = GetRomFile(romPath))
+            {
+                // Get the byaml settings
+                ByamlSettings settings = PrepareByaml(stream);
+
+                // Read the byaml
+                return LoadByamlSerialized<T>(settings, stream);
+            }
+        }
+
         private static ByamlSettings PrepareByaml(Stream stream)
         {
             using (BinaryDataReader reader = new BinaryDataReader(stream, Encoding.ASCII, true))
@@ -152,6 +178,20 @@ namespace JelonzoBot.Blitz
         private static dynamic LoadByamlDynamic(ByamlSettings settings, Stream stream)
         {
             return ByamlFile.Load(stream, settings.supportsPaths, settings.byteOrder);
+        }
+
+        private static T LoadByamlSerialized<T>(ByamlSettings settings, Stream stream)
+        {
+            // Create a ByamlSerializer instance
+            ByamlSerializer serializer = new ByamlSerializer(new ByamlSerializerSettings()
+            {
+                ByteOrder = settings.byteOrder,
+                SupportPaths = settings.supportsPaths,
+                Version = ByamlVersion.Version1 // unused, version is ignored
+            });
+
+            // Deserialize the byaml
+            return serializer.Deserialize<T>(stream);
         }
 
     }
