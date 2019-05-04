4736f7558b155eafe8634ef7de78cc0ba7ceb235
OatmealDome <theoatmealdome@gmail.com>
2019-05-03 21:52:57 -0400

    WebConfig: Add path for Salmon Run data

--- JelonzoBot/Core/Config/JelonzoBotWebConfig.cs
+++ JelonzoBot/Core/Config/JelonzoBotWebConfig.cs
@@ -16,11 +16,18 @@ namespace JelonzoBot.Core.Config
             set;
         }
 
+        public string CoopSettingPath
+        {
+            get;
+            set;
+        }
+
         public override void SetDefaults()
         {
             RemoteServer = null;
             LatestFestivalManifestPath = "/home/oatmealdome/www/latest_festival.json";
             FestivalSettingPath = "/home/oatmealdome/www/festival_{0}.json";
+            CoopSettingPath = "/home/oatmealdome/www/coop.json";
         }
 
     }
