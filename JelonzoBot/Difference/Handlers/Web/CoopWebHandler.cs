46d5e14a2e77602e70d8b3d5ca302fcf5f449d60
OatmealDome <theoatmealdome@gmail.com>
2019-05-03 22:49:57 -0400

    Difference: Add handler to upload CoopSetting JSON

---
+++ JelonzoBot/Difference/Handlers/Web/CoopWebHandler.cs
@@ -0,0 +1,46 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using BcatBotFramework.Core;
+using BcatBotFramework.Core.Config;
+using BcatBotFramework.Difference;
+using JelonzoBot.Blitz;
+using JelonzoBot.Core.Config;
+using Nintendo.Blitz;
+using Nintendo.Blitz.Bcat.Coop;
+
+namespace JelonzoBot.Difference.Handlers.Web
+{
+    public class CoopWebHandler
+    {
+        [JelonzoBotDifferenceHandler(FileType.CoopSetting, DifferenceType.Changed, 1)]
+        public static Task HandleCoop(RomType romType, Dictionary<string, byte[]> data, CoopSetting previousSetting, CoopSetting newSetting, byte[] rawFile)
+        {
+            // Only do this once
+            if (romType != RomType.NorthAmerica)
+            {
+                return Task.FromResult(0);
+            }
+
+            lock (WebFileHandler.Lock)
+            {
+                // Get the WebConfig
+                JelonzoBotWebConfig webConfig = ((JelonzoBotConfiguration)Configuration.LoadedConfiguration).WebConfig;
+
+                // Connect to the remote server if needed
+                WebFileHandler.Connect(webConfig);
+
+                // Deserialize the CoopSetting dynamically
+                dynamic settingDynamic = RomResourceLoader.GetByamlDynamicFromLocal(rawFile);
+
+                // Upload to the server
+                WebFileHandler.WriteSerializedJson(webConfig.CoopSettingPath, settingDynamic);
+
+                // Disconnect
+                WebFileHandler.Disconnect();
+            }
+
+            return Task.FromResult(0);
+        }
+
+    }
+}
