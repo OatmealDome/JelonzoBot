2ebe2d0df0c8d02bc715f97c837b1aedc57089f8
OatmealDome <theoatmealdome@gmail.com>
2019-05-03 21:04:54 -0400

    WebConfig: Add path for the FestivalSetting JSON

--- JelonzoBot/Core/Config/JelonzoBotWebConfig.cs
+++ JelonzoBot/Core/Config/JelonzoBotWebConfig.cs
@@ -10,10 +10,17 @@ namespace JelonzoBot.Core.Config
             set;
         }
 
+        public string FestivalSettingPath
+        {
+            get;
+            set;
+        }
+
         public override void SetDefaults()
         {
             RemoteServer = null;
             LatestFestivalManifestPath = "/home/oatmealdome/www/latest_festival.json";
+            FestivalSettingPath = "/home/oatmealdome/www/festival_{0}.json";
         }
 
     }
