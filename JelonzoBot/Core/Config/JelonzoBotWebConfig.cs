c2f7143591d882b2408e54704a468c322a51c1ee
OatmealDome <theoatmealdome@gmail.com>
2019-06-21 04:21:43 -0400

    Difference: Upload Versus configuration files

--- JelonzoBot/Core/Config/JelonzoBotWebConfig.cs
+++ JelonzoBot/Core/Config/JelonzoBotWebConfig.cs
@@ -22,11 +22,18 @@ namespace JelonzoBot.Core.Config
             set;
         }
 
+        public string VersusSettingPath
+        {
+            get;
+            set;
+        }
+
         public override void SetDefaults()
         {
             RemoteServer = null;
             LatestFestivalManifestPath = "/home/oatmealdome/www/latest_festival.json";
             FestivalSettingPath = "/home/oatmealdome/www/festival_{0}.json";
+            VersusSettingPath = "/home/oatmealdome/www/versus_{0}.json";
             CoopSettingPath = "/home/oatmealdome/www/coop.json";
         }
 
---
+++ JelonzoBot/Difference/Handlers/Web/VersusWebHandler.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using BcatBotFramework.Core;
+using BcatBotFramework.Core.Config;
+using BcatBotFramework.Difference;
+using JelonzoBot.Blitz;
+using JelonzoBot.Core.Config;
+using Nintendo.Blitz;
+using Nintendo.Blitz.Bcat.Versus;
+
+namespace JelonzoBot.Difference.Handlers.Web
+{
+    public class VersusWebHandler
+    {
+        [JelonzoBotDifferenceHandler(FileType.VersusSetting, DifferenceType.Changed, 1)]
+        public static Task HandleVersus(RomType romType, Dictionary<string, byte[]> data, VersusSetting previousSetting, VersusSetting newSetting, byte[] rawFile)
+        {
+            lock (WebFileHandler.Lock)
+            {
+                // Get the WebConfig
+                JelonzoBotWebConfig webConfig = ((JelonzoBotConfiguration)Configuration.LoadedConfiguration).WebConfig;
+
+                // Connect to the remote server if needed
+                WebFileHandler.Connect(webConfig);
+
+                // Deserialize the VersusSetting dynamically
+                dynamic settingDynamic = RomResourceLoader.GetByamlDynamicFromLocal(rawFile);
+
+                // Get the FestivalSetting JSON path
+                string path = webConfig.VersusSettingPath;
+                switch (romType)
+                {
+                    case RomType.NorthAmerica:
+                        path = string.Format(path, "na");
+                        break;
+                    case RomType.Europe:
+                        path = string.Format(path, "eu");
+                        break;
+                    case RomType.Japan:
+                        path = string.Format(path, "jp");
+                        break;
+                    default:
+                        throw new Exception("Invalid RomType");
+                }
+
+                // Upload to the server
+                WebFileHandler.WriteSerializedJson(path, settingDynamic);
+
+                // Disconnect
+                WebFileHandler.Disconnect();
+            }
+
+            return Task.FromResult(0);
+        }
+
+    }
+}
