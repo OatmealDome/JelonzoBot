f07c4ea544a67f323ddb6eb037abf60ce7f31d4c
OatmealDome <theoatmealdome@gmail.com>
2019-06-25 05:02:52 -0400

    RomResourceLoader: Mount pack files and prefer their contents over romfs

--- JelonzoBot/Blitz/RomResourceLoader.cs
+++ JelonzoBot/Blitz/RomResourceLoader.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -6,6 +7,7 @@ using BcatBotFramework.Core;
 using BcatBotFramework.Core.Config;
 using JelonzoBot.Core.Config;
 using LibHac.IO;
+using Nintendo.Archive;
 using Nintendo.Blitz;
 using Nintendo.Switch;
 using Syroot.BinaryData;
@@ -19,6 +21,7 @@ namespace JelonzoBot.Blitz
     {
         private static byte[] Yaz0MagicNumbers = Encoding.ASCII.GetBytes("Yaz0");
         private static NcaWrapper NcaWrapper;
+        private static Dictionary<string, byte[]> PackFiles;
         
         public static void Initialize()
         {
@@ -27,15 +30,53 @@ namespace JelonzoBot.Blitz
 
             // Load the base and update NCAs
             NcaWrapper = new NcaWrapper(KeysetManager.GetKeyset(), romConfig.BaseNcaPath, romConfig.UpdateNcaPath);
+
+            // Create a new pack files  dictionary
+            PackFiles = new Dictionary<string, byte[]>();
+
+            // Load the pack directory
+            IDirectory packDirectory = NcaWrapper.Romfs.OpenDirectory("/Pack", OpenDirectoryMode.Files);
+            
+            // Read every directory entries
+            foreach (DirectoryEntry directoryEntry in packDirectory.Read())
+            {
+                // Load the SARC
+                Sarc sarc = new Sarc(GetRomFileFromRomfs(directoryEntry.FullPath));
+
+                // Loop over every file
+                foreach (KeyValuePair<string, byte[]> pair in sarc)
+                {
+                    // Add this file to the pack files dictionary
+                    PackFiles.Add("/" + pair.Key, pair.Value);
+                }
+            }
         }
 
         public static void Dispose()
         {
             // Dispose of the NcaWrapper
             NcaWrapper.Dispose();
+
+            // Dispose the pack files
+            PackFiles = null;
         }
 
         public static Stream GetRomFile(string romPath)
+        {
+            // Attempt to load from the packs first
+            if (PackFiles.TryGetValue(romPath, out byte[] file))
+            {
+                // Return a MemoryStream
+                return new MemoryStream(file);
+            }
+            else
+            {
+                // Load from the romfs
+                return GetRomFileFromRomfs(romPath);
+            }
+        }
+
+        public static Stream GetRomFileFromRomfs(string romPath)
         {
             // Load the file
             IFile file = NcaWrapper.Romfs.OpenFile(romPath, OpenMode.Read);
